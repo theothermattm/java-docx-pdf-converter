cc7d9f16d60d8c08c1e7c9a748b4b9316f0d7497
Matt Merrill <392778+theothermattm@users.noreply.github.com>
2020-05-13 21:59:48 -0400

    minor cleanup

--- src/main/java/com/rtdschools/Docx2PdfConversion.java
+++ src/main/java/com/rtdschools/Docx2PdfConversion.java
@@ -9,18 +9,18 @@ import org.apache.poi.xwpf.converter.pdf.PdfOptions;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
 /**
- * Taken from https://rdtschools.com/how-to-covert-docx-file-to-pdf-using-apache-poi-library-in-java/
+ * Modified from https://rdtschools.com/how-to-covert-docx-file-to-pdf-using-apache-poi-library-in-java/
  */
 public class Docx2PdfConversion {
     public static void main(String[] args) {
         try (InputStream is = new FileInputStream(new File(args[0]));
              OutputStream out = new FileOutputStream(new File(args[1]));) {
             long start = System.currentTimeMillis();
-// 1) Load DOCX into XWPFDocument
+            // 1) Load DOCX into XWPFDocument
             XWPFDocument document = new XWPFDocument(is);
-// 2) Prepare Pdf options
+            // 2) Prepare Pdf options
             PdfOptions options = PdfOptions.create();
-// 3) Convert XWPFDocument to Pdf
+            // 3) Convert XWPFDocument to Pdf
             PdfConverter.getInstance().convert(document, out, options);
             System.out.println("File converted to a PDF file in :: "
                     + (System.currentTimeMillis() - start) + " milli seconds");
